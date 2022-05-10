11adee4e011bbd0b6b7369e0c2031ae8603dee87
fruitologistero <gsill@LAPTOP-HEKI7EHN>
2022-05-09 22:02:57 -0500

    bomb

--- src/main/java/net/mcreator/neocaverns/init/NeocavernsModItems.java
+++ src/main/java/net/mcreator/neocaverns/init/NeocavernsModItems.java
@@ -13,11 +13,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.neocaverns.item.BombItem;
 import net.mcreator.neocaverns.NeocavernsMod;
 
 public class NeocavernsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, NeocavernsMod.MODID);
 	public static final RegistryObject<Item> ROPE_MIDDLE = block(NeocavernsModBlocks.ROPE_MIDDLE, null);
+	public static final RegistryObject<Item> BOMB = REGISTRY.register("bomb", () -> new BombItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
